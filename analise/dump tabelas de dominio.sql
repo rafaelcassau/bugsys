ac7e416636b4c3bca4f0ecddd9392e149e5d9d3f
rafaelcassau <rafa_cassau@msn.com>
2013-11-27 00:17:12 -0200

    commit finalprojeto

---
+++ analise/dump tabelas de dominio.sql
@@ -0,0 +1,32 @@
+// inserts tabela de dominio employeetype
+
+INSERT INTO employeetype (employeeType) VALUES ("Desenvolvedor");
+INSERT INTO employeetype (employeeType) VALUES ("Testador");
+INSERT INTO employeetype (employeeType) VALUES ("Analista de Sistemas");
+INSERT INTO employeetype (employeeType) VALUES ("Analista de Testes");
+INSERT INTO employeetype (employeeType) VALUES ("Analista de Requisitos");
+INSERT INTO employeetype (employeeType) VALUES ("Web Designer");
+INSERT INTO employeetype (employeeType) VALUES ("DBA");
+INSERT INTO employeetype (employeeType) VALUES ("Arquiteto de Software");
+INSERT INTO employeetype (employeeType) VALUES ("Gerente de Projetos");
+
+// inserts tabela de dominio eventtype
+
+INSERT INTO eventtype (eventType) VALUES ("Erro");
+INSERT INTO eventtype (eventType) VALUES ("Defeito");
+INSERT INTO eventtype (eventType) VALUES ("Falha");
+INSERT INTO eventtype (eventType) VALUES ("Melhoria");
+INSERT INTO eventtype (eventType) VALUES ("Sugestão");
+INSERT INTO eventtype (eventType) VALUES ("Solicitação de Mudança");
+
+// inserts tabela de dominio status
+
+INSERT INTO status (status) VALUES ("Aberto");
+INSERT INTO status (status) VALUES ("Fechado");
+INSERT INTO status (status) VALUES ("Aceito");
+INSERT INTO status (status) VALUES ("Rejeitado");
+INSERT INTO status (status) VALUES ("Corrigido");
+INSERT INTO status (status) VALUES ("Validado");
+INSERT INTO status (status) VALUES ("Não Validado");
+INSERT INTO status (status) VALUES ("Homologado");
+INSERT INTO status (status) VALUES ("Suspenso");
